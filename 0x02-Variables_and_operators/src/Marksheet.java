d2dd5e47c0d2efadef0c156e239b0519c36672c4
Lenson Mutugi <125955780+lensonlovescode@users.noreply.github.com>
2024-10-09 13:34:31 +0300

    Print

---
+++ 0x02-Variables_and_operators/src/Marksheet.java
@@ -0,0 +1,10 @@
+public class Marksheet
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\t\t\t\t\t\tDepartment of Computer Science");
+        System.out.println("\t\t\t\t\t\t\tEnd Semester Results");
+        String line = "-".repeat(89);
+        System.out.println(line);
+    }
+}
